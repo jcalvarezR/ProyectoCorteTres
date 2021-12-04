d066303f290b78c7dc84598fcbc18920e94d5d9a
jcalvarezR <84609046+jcalvarezR@users.noreply.github.com>
2021-12-04 10:24:06 -0500

    Update DashboardGcopias.php
    visualiza la tabla copia y se cambia el estado de la copia

--- DashboardGcopias.php
+++ DashboardGcopias.php
@@ -1,4 +1,6 @@
+
 <?php
+require 'includes/conexion.php';
 session_start();
 
 $varsesion = $_SESSION['email'];
@@ -7,9 +9,15 @@ if ($varsesion == null || $varsesion = '') {
     echo 'Inicie sesion primero';
     die();
 }
+
+    
+$sql = "SELECT * FROM copia";
+$result = $conexion->query($sql);
+    
+    $row = $result->fetch_assoc();
+    
+
 ?>
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,7 +31,7 @@ if ($varsesion == null || $varsesion = '') {
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="src/css/StyleDashboard.css">
-    <title> Administrador</title>
+    <title> Wanote</title>
   </head>
   <body>
     
@@ -38,11 +46,12 @@ if ($varsesion == null || $varsesion = '') {
             </div>
             <div class="menu">
 
-                <a href="DashboardAdmin.php" class="d-block text-light p-3"><i class="icon ion-md-home mr-2 lead"></i>Inicio</a>
+            <a href="DashboardAdmin.php" class="d-block text-light p-3"><i class="icon ion-md-home mr-2 lead"></i>Inicio</a>
                 <a href="DashboardGcopias.php" class="d-block text-light p-3"><i class="icon ion-md-filing"></i> Gestion de copias</a>
                 <a href="DashboardGdocumentos.php" class="d-block text-light p-3"><i class="icon ion-md-bookmarks"></i>  Gestion documento</a>
                 <a href="DashboardGusuarios.php" class="d-block text-light p-3"><i class="icon ion-md-contacts"></i> Gestion de usuarios</a>
                 <a href="includes/salir.php" class="d-block text-light p-3"><i class="icon ion-md-happy"></i> Cerrar sesion</a>
+                
             </div>
         </div>
 
@@ -58,7 +67,8 @@ if ($varsesion == null || $varsesion = '') {
                         <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
                         <button class="btn btn-search position-absolute" type="submit"><i class="icon ion-md-search"></i></button>
                     </form>
-              
+           
+        </div>    
         </nav>
     <!--Seccion de informacion-->
         <div id="content">
@@ -67,33 +77,11 @@ if ($varsesion == null || $varsesion = '') {
                     <div class="row">
                         
                             <div class="col-lg-9">
-                                <h1 class="font-weight-bold mb-0">Bienvenido/a a <b><i>Wanote</i></b> Administrador</h1>
-                                <p class="lead text-muted">Gestione documentos y usuarios.</p>
-                            </div>
-
-                        <!--Boton normal
-                            <div class="col-lg-3 d-flex">
-                                <button class="btn btn-primary w-100 align-self-center">Descargar Reporte</button> 
-                            </div>
-                        -->
-                        <!--
-                        <div class="col-lg-1 d-flex">
-                            <button class="btn btn-primary w-100 align-self-center">Word</button> 
-                        </div>
+                                <h1 class="font-weight-bold mb-0">Bienvenido/a a <b><i>Wanote</i></b></h1>
+                                <p class="lead text-muted">Podra publicar y pedir los documentos y libros que desee, disfrute de un contenido exclusivo
+                                e interesante.</p>
+                            </div>                        
                         
-                        <div class="col-lg-1 d-flex">
-                            <button class="btn btn-primary w-100 align-self-center">Excel</button>
-                            
-                        </div>
-                        <div class="col-lg-1 d-flex">
-                            <button class="btn btn-primary w-100 align-self-center">PDF</button>
-                            
-                        </div>
--->
-                        
-                        
-                        
-
                     </div>
                 </div>
             </section>
@@ -148,178 +136,38 @@ if ($varsesion == null || $varsesion = '') {
                     <br>
 
 
-
 <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Gestion copias</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Pedir copias</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
-                                        <tr>
+                                    <tr>
                                             <th>id_copia</th>
                                             <th>id_documento</th>
                                             <th>nombre</th>
-                                            <th>id_estado_copia</th>
+                                            <th>id_estado_documento</th>
                                             <th>fecha_prestamo</th>
-                                            
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>id_copia</th>
-                                            <th>id_documento</th>
-                                            <th>nombre</th>
-                                            <th>id_estado_copia</th>
-                                            <th>fecha_prestamo</th>
-                                            
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
+                                            <th>tipo_copia</th>
+                                           
                                             
                                         </tr>
                                         <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Ashton Cox</td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Cedric Kelly</td>
-                                            <td>Senior Javascript Developer</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2012/03/29</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Airi Satou</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>33</td>
-                                            <td>2008/11/28</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Brielle Williamson</td>
-                                            <td>Integration Specialist</td>
-                                            <td>New York</td>
-                                            <td>61</td>
-                                            <td>2012/12/02</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Herrod Chandler</td>
-                                            <td>Sales Assistant</td>
-                                            <td>San Francisco</td>
-                                            <td>59</td>
-                                            <td>2012/08/06</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Rhona Davidson</td>
-                                            <td>Integration Specialist</td>
-                                            <td>Tokyo</td>
-                                            <td>55</td>
-                                            <td>2010/10/14</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Colleen Hurst</td>
-                                            <td>Javascript Developer</td>
-                                            <td>San Francisco</td>
-                                            <td>39</td>
-                                            <td>2009/09/15</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Sonya Frost</td>
-                                            <td>Software Engineer</td>
-                                            <td>Edinburgh</td>
-                                            <td>23</td>
-                                            <td>2008/12/13</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Jena Gaines</td>
-                                            <td>Office Manager</td>
-                                            <td>London</td>
-                                            <td>30</td>
-                                            <td>2008/12/19</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Quinn Flynn</td>
-                                            <td>Support Lead</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2013/03/03</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Charde Marshall</td>
-                                            <td>Regional Director</td>
-                                            <td>San Francisco</td>
-                                            <td>36</td>
-                                            <td>2008/10/16</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Haley Kennedy</td>
-                                            <td>Senior Marketing Designer</td>
-                                            <td>London</td>
-                                            <td>43</td>
-                                            <td>2012/12/18</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Tatyana Fitzpatrick</td>
-                                            <td>Regional Director</td>
-                                            <td>London</td>
-                                            <td>19</td>
-                                            <td>2010/03/17</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Michael Silva</td>
-                                            <td>Marketing Designer</td>
-                                            <td>London</td>
-                                            <td>66</td>
-                                            <td>2012/11/27</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Paul Byrd</td>
-                                            <td>Chief Financial Officer (CFO)</td>
-                                            <td>New York</td>
-                                            <td>64</td>
-                                            <td>2010/06/09</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td>Gloria Little</td>
-                                            <td>Systems Administrator</td>
-                                            <td>New York</td>
-                                            <td>59</td>
-                                            <td>2009/04/10</td>
-                                            
+                                       <?php     
+                                        echo "<tr>
+                                                <td>".$row['id_copia']."</td>
+                                                <td>".$row['id_documento']."</td>
+                                                <td>".$row['nombre']."</td>
+                                                <td>".$row['id_estado_copia']."</td>
+                                                <td>".$row['fecha_prestamo']."</td>
+                                                <td>".$row['tipo_copia']."</td>
+                                                <td> <a href='cambiarEstadoCopia.php?id=".$row['id_copia']."'>Estado</a> </td>
+                                            </tr>";  
+
+                                                ?>
+
                                         </tr>
                                     </tbody>
                                 </table>
@@ -329,8 +177,7 @@ if ($varsesion == null || $varsesion = '') {
 
 
 
-
-                    
+                </div>
             </section>
         </div>
     <!---->
