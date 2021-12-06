1bcbb51d479ddc627b3a098daee466a04a0c4a9e
jcalvarezR <84609046+jcalvarezR@users.noreply.github.com>
2021-12-06 00:50:50 -0500

    Update guardarDoc.php
    sql guardar doc con copias

--- includes/guardarDoc.php
+++ includes/guardarDoc.php
@@ -1,10 +1,11 @@
 <?php
-include "conexion.php";
-$mysqli = new mysqli("sql10.freesqldatabase.com", "sql10454351","ZRPawxLMAg","sql10454351");
+require 'conexion.php';
+session_start();
+//guardado de documento
 
-$id = $_POST['id_documento'];
 $editorial = $_POST['id_editorial'];
-$id_usuario = $_POST['id_usuario'];
+$idUsuario = $_SESSION['id'];
+$numero_copias=$_POST['numero_copias'];
 $tipo_documento = $_POST['id_tipo_documento'];
 $nombre = $_POST['nombre'];
 $descripcion = $_POST['descripcion'];
@@ -12,16 +13,34 @@ $fecha_publicacion = $_POST['fecha_publicacion'];
 $isbn = $_POST['isbn'];
 $ssn = $_POST['ssn'];
 $adicional = $_POST['adicional'];
+$tipo_copia = $_POST['tipo_copia'];
 
 
 
-$sql = "INSERT INTO documento(id_documento,id_editorial,id_usuario,id_tipo_documento,nombre,descripcion,fecha_publicacion,isbn,ssn,adicional) 
-      VALUES ('$id','$editorial','$id_usuario','$tipo_documento','$nombre',' $descripcion','$fecha_publicacion','$isbn','$ssn','$adicional')";
-$resultado = $mysqli->query($sql);
+$sql = "INSERT INTO documento(id_editorial,id_usuario,id_tipo_documento,nombre,descripcion,fecha_publicacion,isbn,ssn,adicional) 
+      VALUES ('$editorial','$idUsuario','$tipo_documento','$nombre',' $descripcion','$fecha_publicacion','$isbn','$ssn','$adicional')";
+$resultado = $conexion->query($sql);
 
-		
+		//guardado de copias
+$sql = "SELECT * from documento where id_editorial = '$editorial'and id_usuario = '$idUsuario' and 
+id_tipo_documento='$tipo_documento' and
+ nombre='$nombre' and
+ 
+  fecha_publicacion='$fecha_publicacion'and 
+  isbn='$isbn'
+  and ssn='$ssn'
+  and adicional='$adicional'";	
 
-echo "<meta http-equiv='refresh' content='0; url=../Dashboard.php'>";
+$result = $conexion->query($sql);
+$row = $result->fetch_assoc();
+$id_documento=$row["id_documento"];
+$fecha=date('y-m-d h:i:s');
+for ($i = 0; $i < $numero_copias; $i++) {
+   $sql= "INSERT into copia(id_documento,id_estado_copia,fecha_prestamo,tipo_copia)
+   VALUES('$id_documento','1','$fecha','$tipo_copia')";
+   $resultado = $conexion->query($sql);
+}
+echo "<meta http-equiv='refresh' content='0; url=../DashboardUsuario.php'>";
 
 //echo "<meta http-equiv='refresh' content='0; url=nuevo.php'>";
 ?>
